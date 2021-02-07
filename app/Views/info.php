87568ff10a7e2743ff3f3773d5fd6c44908425f5
Jericho <edwardk753@gmail.com>
2021-02-07 19:13:58 +0700

    CRUD

---
+++ app/Views/info.php
@@ -0,0 +1,16 @@
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->section('content'); ?>
+
+<h3>Info Kegiatan</h3>
+<p>Informasi kegiatan siswa bulan ini :</p>
+<ul>
+                <li>10 Agustus - Masa Orientasi Siswa</li>
+                <li>17 Agustus - Upacara Kemerdekaan</li>
+</ul>
+<p>Infomasi Kegiatan Siswa Bulan Depan</p>
+<ul>
+                <li>12 September - Ujian Tengah Semester</li>
+</ul>
+
+<?= $this->endSection(); ?>
